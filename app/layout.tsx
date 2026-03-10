b9c2ef48984260a7b131af9131d9a916e4b55f0b
Joanthas Fiscarelli <joanthasfiscarelli@Mac-mini-de-Joanthas.local>
2026-03-10 03:47:11 -0300

    corrige ID do Meta Pixel

--- app/layout.tsx
+++ app/layout.tsx
@@ -43,7 +43,7 @@ export default function RootLayout({
             t.src=v;s=b.getElementsByTagName(e)[0];
             s.parentNode.insertBefore(t,s)}(window, document,'script',
             'https://connect.facebook.net/en_US/fbevents.js');
-            fbq('init', '1794736248583286');
+            fbq('init', '1630841615029234');
             fbq('track', 'PageView');
           `}
         </Script>
@@ -52,7 +52,7 @@ export default function RootLayout({
             height="1" 
             width="1" 
             style={{display: 'none'}}
-            src="https://www.facebook.com/tr?id=1794736248583286&ev=PageView&noscript=1"
+            src="https://www.facebook.com/tr?id=1630841615029234&ev=PageView&noscript=1"
             alt=""
           />
         </noscript>
